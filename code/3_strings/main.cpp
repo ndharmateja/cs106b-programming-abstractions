755ba6aedaea6b54471c56fd107f125381ad4a09
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-29 14:57:01 +0530

    refactor folder structure

--- code/3_strings/main.cpp
+++ code/3_strings/main.cpp
@@ -10,19 +10,36 @@
 #include "strlib.h"
 using namespace std;
 
-
 int main()
 {
+    string s = "dharma teja";
+    print_string(s);
+    s.erase(2, 2);
+    print_string(s);
+    s.append(" nuli");
+    print_string(s);
+
+    string s1 = "world";
+    string s2 = "hello" + s1;
+    print_string(s1);
+    print_string(s2);
+
+    string s3 = "hi" + '?';
+    print_string(s3);
+
     ifstream input;
     input.open("data.txt");
     string line;
     int total = 0;
-    while(getline(input, line)) {
-        if (stringIsInteger(line)){
+    while (getline(input, line))
+    {
+        if (stringIsInteger(line))
+        {
             total += stringToInteger(line);
         }
     }
     cout << "total: " << total << endl;
     input.close();
+
     return 0;
 }
