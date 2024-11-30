c4aaaa1a6a08436551f72a31378bc74d204c2d31
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-30 19:31:27 +0530

    format file

--- assignments/assignment1/src/PlayingFair.cpp
+++ assignments/assignment1/src/PlayingFair.cpp
@@ -19,42 +19,47 @@ string bSequenceOfOrder(int n) {
     return "";
 }
 
-
-
-
-
-
 /* * * * * * Provided Test Cases * * * * * */
 
-PROVIDED_TEST("Sequences of order 3 are correct.") {
+PROVIDED_TEST("Sequences of order 3 are correct.")
+{
     /* Some very basic checks. */
     EXPECT_EQUAL(aSequenceOfOrder(3), "ABBABAAB");
     EXPECT_EQUAL(bSequenceOfOrder(3), "BAABABBA");
 }
 
-PROVIDED_TEST("Only characters should be As and Bs.") {
-    for (int i = 0; i < 10; i++) {
-        for (char ch: aSequenceOfOrder(i)) {
+PROVIDED_TEST("Only characters should be As and Bs.")
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (char ch : aSequenceOfOrder(i))
+        {
             EXPECT(ch == 'A' || ch == 'B');
         }
     }
 }
 
-PROVIDED_TEST("A-sequence of positive order should have equal As and Bs.") {
+PROVIDED_TEST("A-sequence of positive order should have equal As and Bs.")
+{
     /* Exclude the sequence of order 0, which is just a single character. */
-    for (int i = 1; i < 10; i++) {
+    for (int i = 1; i < 10; i++)
+    {
         int as = 0;
         int bs = 0;
-        for (char ch: aSequenceOfOrder(i)) {
-            if (ch == 'A') as++;
-            else bs++;
+        for (char ch : aSequenceOfOrder(i))
+        {
+            if (ch == 'A')
+                as++;
+            else
+                bs++;
         }
 
         EXPECT_EQUAL(as, bs);
     }
 }
 
-PROVIDED_TEST("Triggers error on negative inputs.") {
+PROVIDED_TEST("Triggers error on negative inputs.")
+{
     /* The EXPECT_ERROR macro expects the given expression to call error(). Remember that
      * you need to guard against invalid inputs.
      */
@@ -76,11 +81,3 @@ PROVIDED_TEST("Triggers error on negative inputs.") {
  *
  * Happy testing!
  */
-
-
-
-
-
-
-
-
