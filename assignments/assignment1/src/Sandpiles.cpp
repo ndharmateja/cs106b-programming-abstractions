ab44ce2f4f36ebc374756f39b685c52aa294cc6d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-30 20:20:38 +0530

    format code

--- assignments/assignment1/src/Sandpiles.cpp
+++ assignments/assignment1/src/Sandpiles.cpp
@@ -20,52 +20,65 @@ void dropSandOn(Grid<int>& world, int row, int col) {
 
 /* * * * * * Provided Test Cases * * * * * */
 
-PROVIDED_TEST("Dropping into an empty cell only changes that cell.") {
+PROVIDED_TEST("Dropping into an empty cell only changes that cell.")
+{
     /* Create a simple source grid. */
     Grid<int> before = {
-        { 3, 3, 3 },
-        { 3, 0, 3 },
-        { 3, 3, 3 }
-    };
+        {3, 3, 3},
+        {3, 0, 3},
+        {3, 3, 3}};
     Grid<int> after = {
-        { 3, 3, 3 },
-        { 3, 1, 3 },
-        { 3, 3, 3 }
-    };
+        {3, 3, 3},
+        {3, 1, 3},
+        {3, 3, 3}};
 
     dropSandOn(before, 1, 1);
     EXPECT_EQUAL(before, after); // The above call changes 'before.'
 }
 
-PROVIDED_TEST("Non-chaining topples work.") {
+PROVIDED_TEST("Non-chaining topples work.")
+{
     /* Create a simple source grid. */
     Grid<int> before = {
-        { 0, 0, 0 },
-        { 1, 3, 1 },
-        { 0, 2, 0 }
-    };
+        {0, 0, 0},
+        {1, 3, 1},
+        {0, 2, 0}};
     Grid<int> after = {
-        { 0, 1, 0 },
-        { 2, 0, 2 },
-        { 0, 3, 0 }
-    };
+        {0, 1, 0},
+        {2, 0, 2},
+        {0, 3, 0}};
 
     dropSandOn(before, 1, 1);
     EXPECT_EQUAL(before, after); // The above call changes 'before.'
 }
 
-PROVIDED_TEST("Two topples chain.") {
+PROVIDED_TEST("Two topples chain.")
+{
     /* Create a simple source grid. */
     Grid<int> before = {
-        { 0, 0, 0, 0 },
-        { 0, 3, 3, 0 },
-        { 0, 0, 0, 0 }
-    };
+        {0, 0, 0, 0},
+        {0, 3, 3, 0},
+        {0, 0, 0, 0}};
     Grid<int> after = {
-        { 0, 1, 1, 0 },
-        { 1, 1, 0, 1 },
-        { 0, 1, 1, 0 }
-    };
+        {0, 1, 1, 0},
+        {1, 1, 0, 1},
+        {0, 1, 1, 0}};
+
+    dropSandOn(before, 1, 1);
+    EXPECT_EQUAL(before, after); // The above call changes 'before.'
+}
+
+PROVIDED_TEST("Interesting.")
+{
+    /* Create a simple source grid. */
+    Grid<int> before = {
+        {3, 3, 2},
+        {2, 3, 3},
+        {0, 2, 3}};
+    Grid<int> after = {
+        {1, 3, 0},
+        {1, 0, 3},
+        {2, 1, 1}};
 
     dropSandOn(before, 1, 1);
     EXPECT_EQUAL(before, after); // The above call changes 'before.'
@@ -85,13 +98,3 @@ PROVIDED_TEST("Two topples chain.") {
  *
  * Happy testing!
  */
-
-
-
-
-
-
-
-
-
-
