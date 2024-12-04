66c180aa69d3d036ee71a68a4f0cbae3141698ad
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-12-04 18:07:58 +0530

    add fns to print permutations of decimal, hexadecimal

--- code/14_backtracking/main.cpp
+++ code/14_backtracking/main.cpp
@@ -8,32 +8,67 @@
 #include <iostream>
 using namespace std;
 
+void printDigit(int digit)
+{
+    if (digit < 10)
+        cout << digit;
+    else if (digit == 10)
+        cout << "A";
+    else if (digit == 11)
+        cout << "B";
+    else if (digit == 12)
+        cout << "C";
+    else if (digit == 13)
+        cout << "D";
+    else if (digit == 14)
+        cout << "E";
+    else if (digit == 15)
+        cout << "F";
+}
 
-void printBinary(int digits, Vector<int> &prefix)
+void printPermutations(int digits, Vector<int> &prefix, int base)
 {
     if (digits == 0)
     {
-        for (int bit : prefix)
-            cout << bit;
+        for (int digit : prefix)
+            printDigit(digit);
         cout << endl;
         return;
     }
 
-    prefix.add(0);
-    printBinary(digits-1, prefix);
-    prefix.remove(prefix.size() - 1);
-    prefix.add(1);
-    printBinary(digits-1, prefix);
-    prefix.remove(prefix.size() - 1);
+    for (int i = 0; i < base; ++i)
+    {
+        prefix.add(i);
+        printPermutations(digits - 1, prefix, base);
+        prefix.remove(prefix.size() - 1);
+    }
 }
 
-void printBinary(int digits) {
+void printBinary(int digits)
+{
     Vector<int> v;
-    printBinary(digits, v);
+    printPermutations(digits, v, 2);
+}
+
+void printDecimal(int digits)
+{
+    Vector<int> v;
+    printPermutations(digits, v, 10);
+}
+
+void printHexadecimal(int digits)
+{
+    Vector<int> v;
+    printPermutations(digits, v, 16);
 }
 
 int main()
 {
-    printBinary(3);
+    cout << "printBinary(2):" << endl;
+    printBinary(2);
+    cout << "printDecimal(3):" << endl;
+    printDecimal(3);
+    cout << "printHexdecimal(2):" << endl;
+    printHexadecimal(2);
     return 0;
 }
